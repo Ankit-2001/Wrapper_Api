3653130b7458b2fb3726d4c7e1bdf65ff70523ba
Ankit Kumar <ankit.kumar.28.1@mountblue.tech>
2024-06-28 12:54:54 +0530

    Adding input as payload to wrapper APIs

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -4,8 +4,66 @@ const url = "https://north-america.api.capillarytech.com/v2/customers";
 
 export async function addCustomer(req, res) {
   const accessToken = req.headers["x-cap-api-oauth-token"];
-  const data = req.body;
 
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+ const identifiers = req.body.attributeSets[0].customerIdentifiers.map(
+   (identifier) => {
+     return {
+       type: identifier.type, 
+       value:
+         identifier.type === "email"
+           ? `${identifier.value}@gmail.com`
+           : identifier.value,
+     };
+   }
+ );
+  const data = {
+    associatedWith: "bukl.till",
+    profiles: [
+      {
+        firstName: `${firstName}`,
+        lastName: `${lastName}`,
+
+        identifiers: identifiers,
+        commChannels: [
+          {
+            type: "mobile",
+            value: "6783947457",
+            primary: "true",
+            verified: "true",
+            meta: {
+              residence: "true",
+            },
+          },
+          {
+            type: "email",
+            value: "serena211darim@gmail.com",
+            primary: "true",
+            verified: "true",
+            meta: {
+              residence: "true",
+            },
+          },
+        ],
+        fields: {
+          employee: "true",
+          dateofbirth: "22-10-2000",
+        },
+        source: "INSTORE",
+        accountId: "websiteBPCLSF",
+      },
+    ],
+    extendedFields: {
+      gender: "Female",
+
+      city: "Sattari",
+      state: "GOA",
+      area: "House no 2051,opposite  Sesa Goa  Colony,",
+      zip: "403505",
+    },
+    referralCode: "",
+  };
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +73,7 @@ export async function addCustomer(req, res) {
 
   try {
     const responseData = await axios.post(url, data, config);
-    res.status(200).json({ msg: responseData.data });
+    res.status(200).json(responseData.data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -26,7 +84,6 @@ export async function getCustomerDetails(req, res) {
   const source = "INSTORE";
 
   const getUrl = `${url}/${customerId}?source=${source}`;
-  console.log(getUrl);
   const accessToken = req.headers["x-cap-api-oauth-token"];
 
   const config = {
